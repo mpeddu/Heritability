4f85bb91cb9e99c59560b593dd06e958b4621082
Mohan <49529758+mpeddu@users.noreply.github.com>
2025-08-01 11:58:10 +0200

    Update H2 Cullis.R
    Syntax changed to reflect ASReml 4.x.xxx

--- Alternative Heritability Measures/ASReml-R/H2 Cullis.R
+++ Alternative Heritability Measures/ASReml-R/H2 Cullis.R
@@ -18,11 +18,11 @@ g.ran <- asreml(fixed = yield ~       rep,
 # Handle model estimates #
 ##########################
 # Genetic variance component
-vc.g <- summary(g.ran)$varcomp['gen!gen.var','component']
+vc.g <- summary(g.ran)$varcomp['gen','component']
 vc.g #0.142902
 
 # Mean variance of a difference of two genotypic BLUPs
-vdBLUP.mat <- predict(g.ran, classify="gen", only="gen", sed=TRUE)$pred$sed^2 # obtain squared s.e.d. matrix 
+vdBLUP.mat <- predict(g.ran, classify="gen", only="gen", sed=TRUE)$sed^2 # obtain squared s.e.d. matrix 
 vdBLUP.avg <- mean(vdBLUP.mat[upper.tri(vdBLUP.mat, diag=FALSE)]) # take mean of upper triangle
 vdBLUP.avg #0.05455038
 
