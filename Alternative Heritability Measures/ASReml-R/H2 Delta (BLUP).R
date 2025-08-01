4c114b32415faee9978fa27003f0a1deb5dedf95
Mohan <49529758+mpeddu@users.noreply.github.com>
2025-08-01 12:20:41 +0200

    Update H2 Delta (BLUP).R
    Syntax changed to reflect ASReml 4.x.xxx

--- Alternative Heritability Measures/ASReml-R/H2 Delta (BLUP).R
+++ Alternative Heritability Measures/ASReml-R/H2 Delta (BLUP).R
@@ -15,7 +15,7 @@ g.ran <- asreml(fixed = yield ~       rep,
                 data=dat)
 
 # BLUPs for genotype main effect
-g.pred  <- predict(g.ran, classify="gen", only="gen", sed=T, vcov=T)$pred
+g.pred  <- predict(g.ran, classify="gen", only="gen", sed=T, vcov=T)
 BLUPs.g <- data.table(g.pred$pvals[,c(1,2)]); names(BLUPs.g) <- c("gen","BLUP")
 
 ##########################
@@ -27,13 +27,13 @@ list.g <- levels(dat$gen) # list of genotype names
 n.g    <- length(list.g)  # number of genotypes
 
 # G.g (i.e. estimated G matrix of genotype main effect)
-vc.g <- summary(g.ran)$varcomp['gen!gen.var','component'] # VC genotype main effect
+vc.g <- summary(g.ran)$varcomp['gen','component'] # VC genotype main effect
 G.g.wide <- diag(1, n.g) * vc.g; dimnames(G.g.wide) <- list(list.g, list.g) # G.g matrix
 G.g.long <- data.table(reshape::melt(G.g.wide)); names(G.g.long) <- c("gen1","gen2","sigma") # G.g matrix in long format
 
 # Variance of a difference between genotypic BLUPs (based on C22.g/PEV matrix)
 vd.g.wide <- g.pred$sed^2; dimnames(vd.g.wide) <- list(list.g, list.g) # C22.g matrix
-vd.g.long <- data.table(reshape::melt(vd.g.wide)); names(vd.g.long) <- c("gen1","gen2","vd") # C22.g matrix in long format
+vd.g.long <- data.table(reshape::melt(as.matrix(vd.g.wide))); names(vd.g.long) <- c("gen1","gen2","vd") # C22.g matrix in long format
 
 # merge BLUPs, G.g and C22.g information into "H2D.blup" table
 g.var <- G.g.long[gen1==gen2, .(gen1, sigma)]; names(g.var) <- c("gen","var") # variances G.g
